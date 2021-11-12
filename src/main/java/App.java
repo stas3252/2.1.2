eb2208bca3df814abac0a6e30aeaae3f9aa6e610
stas3252 <stanislav.kosyy@yandex.ru>
2021-11-12 20:39:38 +0300

    first send

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,10 +3,18 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 public class App {
     public static void main(String[] args) {
-        ApplicationContext applicationContext =
-                new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        HelloWorld beanH1 = (HelloWorld) applicationContext.getBean("HelloWorld");
+        System.out.println(beanH1.getMessage());
+        HelloWorld beanH2 = (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(beanH2.getMessage());
+
+        Cat beanC1 = (Cat) applicationContext.getBean("cat");
+        System.out.println(beanC1.getMessage());
+        Cat beanC2 = (Cat) applicationContext.getBean("cat");
+        System.out.println(beanC2.getMessage());
+        System.out.println("beanH1 == beanH2? " + (beanH1 == beanH2));
+        System.out.println("beanC1 == beanC2? " + (beanC1 == beanC2));
     }
 }
